333eae084cf97c32ad34ec79bca877a5937436da
Pavel <neverfearnevermore1@gmail.com>
2022-02-09 21:51:17 +0300

    feat: update script reverse int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
-}
+  const numToStr = Math.abs(n).toString();
+  const splitStr = numToStr.split('');
+  const revStr = splitStr.reverse();
+  const joinStr = revStr.join("");
+return joinStr;
+};
